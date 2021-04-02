ea159ef49bc158b2d72f6d33623347c60a697746
skrash <45307480+skrash@users.noreply.github.com>
2021-04-02 16:22:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ class Stack():
         s = Stack(self.data, self.prev)
         self.prev = s
         self.data = val
+        
+# version 2
+import copy
+
+class Stack():
+    def __init__(self, val, prev=None):
+        self.prev = prev
+        self.data = val
+    def Next(self, val):
+        self.__init__(val, copy.deepcopy(self))
